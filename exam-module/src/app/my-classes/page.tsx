86cecb7acd61f5150ffe1ef0d7aca17b9e0b937e
Urantogos O <ourantogos65@gmail.com>
2026-03-26 21:10:52 -0700

    add avatar

--- exam-module/src/app/my-classes/page.tsx
+++ exam-module/src/app/my-classes/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import React from "react";
-import { useGetClassesQuery } from "@/gql/graphql";
-import { LayoutDashboard, Plus } from "lucide-react";
+import { useGetClassesQuery, useGetStudentsQuery } from "@/gql/graphql";
+import { Plus } from "lucide-react";
 import { ClassCard } from "./_components/ClassCard";
 
 export default function ClassesPage() {
   const { data, loading, error } = useGetClassesQuery();
+  const { data: studentData } = useGetStudentsQuery();
 
   if (loading)
     return (
@@ -14,6 +15,7 @@ export default function ClassesPage() {
         Уншиж байна...
       </div>
     );
+
   if (error)
     return (
       <div className="ml-64 p-10 text-red-500">
@@ -22,11 +24,16 @@ export default function ClassesPage() {
     );
 
   const classes = data?.getClasses || [];
+  const students = studentData?.getStudents || [];
+
+  const getStudentCount = (classId: string) => {
+    return students.filter((s) => s.classId === classId).length;
+  };
 
   return (
-    <div className="min-h-screen  w-full">
-      <main className="flex-1  flex flex-col">
-        <div className="p-8 max-w-7xl ">
+    <div className="min-h-screen w-full">
+      <main className="flex-1 flex flex-col">
+        <div className="p-8 max-w-9xl">
           <div className="flex justify-between items-start mb-10">
             <div>
               <h2 className="text-3xl font-bold text-gray-800 mb-2">
@@ -37,6 +44,7 @@ export default function ClassesPage() {
                 удирдана.
               </p>
             </div>
+
             <button className="bg-[#5136a8] hover:bg-[#432c8a] text-white px-5 py-2.5 rounded-xl flex items-center gap-2 transition font-medium shadow-md shadow-purple-200">
               <Plus size={20} />
               Анги нэмэх
@@ -55,6 +63,7 @@ export default function ClassesPage() {
                   id={item.id}
                   name={item.name}
                   index={index}
+                  studentCount={getStudentCount(item.id)}
                 />
               ))}
             </div>
